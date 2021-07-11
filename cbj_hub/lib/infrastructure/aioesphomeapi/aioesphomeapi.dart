4393732ab9c2c2fba3c37a6e82d3b81eb4d14346
Guy Luz <guyluz11@gmail.com>
2021-07-12 00:21:53 +0300

    Saving unsuccessful secure connection to esphome device

--- cbj_hub/lib/infrastructure/aioesphomeapi/aioesphomeapi.dart
+++ cbj_hub/lib/infrastructure/aioesphomeapi/aioesphomeapi.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:cbj_hub/infrastructure/gen/aioesphomeapi/protoc_as_dart/aioesphomeapi/api.pbgrpc.dart';
 import 'package:grpc/grpc.dart';
 
@@ -10,22 +13,76 @@ class AioEspHomeApi {
   ///  Turn smart device on
   static Future<void> createStreamWithHub(String addressToHub) async {
     channel = await createCbjHubClient(addressToHub);
-    stub = APIConnectionClient(channel!);
-    ResponseFuture<DeviceInfoResponse> response;
-    DeviceInfoRequest deviceInfoRequest = DeviceInfoRequest();
+
+    DeviceInfoResponse response = DeviceInfoResponse();
+    HelloRequest helloRequest = HelloRequest(clientInfo: 'aioesphomeapi');
+    final ConnectRequest connectRequest = ConnectRequest();
+    DisconnectRequest disconnectRequest = DisconnectRequest.create();
     try {
-      response = stub!.device_info(deviceInfoRequest);
+      // print('Now');
+      // final DisconnectResponse b =
+      //     await APIConnectionClient(channel!).disconnect(disconnectRequest);
+      // print('Hello $b');
+      final ConnectResponse a = await APIConnectionClient(channel!).connect(
+        connectRequest,
+      );
+      print('Connect Response $a');
+
+      // stub = APIConnectionClient(
+      //   channel!,
+      // );
+      // final DeviceInfoRequest deviceInfoRequest = DeviceInfoRequest.create();
+
+      // response = await stub!.device_info(deviceInfoRequest);
     } catch (e) {
       print('Caught error: $e');
       await channel?.shutdown();
     }
+    print('response:${response.name.toString()}');
   }
 
   static Future<ClientChannel> createCbjHubClient(String deviceIp) async {
     await channel?.shutdown();
-    return ClientChannel(deviceIp,
-        port: 6053,
-        options:
-            const ChannelOptions(credentials: ChannelCredentials.insecure()));
+
+    final String myPath =
+        '/home/guy/Documents/programming/git/CBJ_Smart-Home/CBJ_Hub/cbj_hub/lib/infrastructure/aioesphomeapi/cacert.pem';
+    final File f = File(myPath);
+    final Uint8List trustedRoot = f.readAsBytesSync();
+
+    final channelCredentials =
+        ChannelCredentials.secure(certificates: trustedRoot);
+    final channelOptions = ChannelOptions(credentials: channelCredentials);
+
+    return channel = ClientChannel(
+      deviceIp,
+      port: 6053,
+      options: channelOptions,
+    );
+
+    // final channel2 = ClientChannel(deviceIp,
+    //     port: 6053,
+    //     options: ChannelOptions(
+    //       credentials: ClientCertificateChannelCredentials(cert, key, pass),
+    //     ));
+  }
+}
+
+class ClientCertificateChannelCredentials extends ChannelCredentials {
+  final List<int> _cert;
+  final List<int> _key;
+  final String _pass;
+
+  const ClientCertificateChannelCredentials(
+    this._cert,
+    this._key,
+    this._pass,
+  ) : super.secure();
+
+  @override
+  SecurityContext get securityContext {
+    return SecurityContext(withTrustedRoots: true)
+      ..useCertificateChainBytes(_cert)
+      ..usePrivateKeyBytes(_key, password: _pass)
+      ..setAlpnProtocols(supportedAlpnProtocols, false);
   }
 }
