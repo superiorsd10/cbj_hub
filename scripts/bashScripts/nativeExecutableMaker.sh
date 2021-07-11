dc9cb74ff98dc7733a403ded89f2630236c2bd9c
Guy Luz <guyluz11@gmail.com>
2021-07-12 00:46:57 +0300

    Starting adding the snap files

---
+++ scripts/bashScripts/nativeExecutableMaker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Making native executable of the project"
+
+scripts/bashScripts/dartSdkDownload.sh # Downloading dart-sdk for the correct architecture.
+
+unzip dartsdk-*.zip
+rm dartsdk-*.zip
+
+cd cbj_hub
+../dart-sdk/bin/pub get --no-precompile
+#sudo ../dart-sdk/bin/pub run build_runner build --delete-conflicting-outputs
+cd ..
+
+#chmod -R +rx dart-sdk/
+dart-sdk/bin/dart2native cbj_hub/bin/main.dart -o CyBear-Jinni_Hub
+# sudo dart-sdk/bin/dart CyBear-Jinni_Hub/bin/main.dart .
+
+rm -r dart-sdk/
