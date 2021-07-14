35b5faee234d4e66e8a42616584e60cfc2e9708a
Guy Luz <guyluz11@gmail.com>
2021-07-14 13:31:03 +0300

    Changed ESPHome client to user sockets instead of grpc

--- cbj_hub/lib/infrastructure/aioesphomeapi/aioesphomeapi.dart
+++ cbj_hub/lib/infrastructure/aioesphomeapi/aioesphomeapi.dart
@@ -1,88 +1,53 @@
 import 'dart:io';
 import 'dart:typed_data';
 
-import 'package:cbj_hub/infrastructure/gen/aioesphomeapi/protoc_as_dart/aioesphomeapi/api.pbgrpc.dart';
-import 'package:grpc/grpc.dart';
-
 class AioEspHomeApi {
-  static ClientChannel? channel;
-  static APIConnectionClient? stub;
-
-  static deviceInfo() {}
-
   ///  Turn smart device on
-  static Future<void> createStreamWithHub(String addressToHub) async {
-    channel = await createCbjHubClient(addressToHub);
+  static Future<void> createStreamWithHub(String addressToServer) async {
+    // connect to the socket server
+    final socket = await Socket.connect(addressToServer, 6053);
+    print('Connected to: ${socket.remoteAddress.address}:${socket.remotePort}');
 
-    DeviceInfoResponse response = DeviceInfoResponse();
-    HelloRequest helloRequest = HelloRequest(clientInfo: 'aioesphomeapi');
-    final ConnectRequest connectRequest = ConnectRequest();
-    DisconnectRequest disconnectRequest = DisconnectRequest.create();
-    try {
-      // print('Now');
-      // final DisconnectResponse b =
-      //     await APIConnectionClient(channel!).disconnect(disconnectRequest);
-      // print('Hello $b');
-      final ConnectResponse a = await APIConnectionClient(channel!).connect(
-        connectRequest,
-      );
-      print('Connect Response $a');
+    final message = Uint8List(8);
+    final bytedata = ByteData.view(message.buffer);
 
-      // stub = APIConnectionClient(
-      //   channel!,
-      // );
-      // final DeviceInfoRequest deviceInfoRequest = DeviceInfoRequest.create();
+    bytedata.setUint8(0, 0x00);
+    bytedata.setUint8(1, 0x07);
+    bytedata.setUint8(2, 0xFF);
+    bytedata.setUint8(3, 0x88);
+    bytedata.setUint8(4, 0x88);
+    bytedata.setUint8(5, 0x88);
+    bytedata.setUint8(6, 0x88);
+    bytedata.setUint8(7, 0x88);
 
-      // response = await stub!.device_info(deviceInfoRequest);
-    } catch (e) {
-      print('Caught error: $e');
-      await channel?.shutdown();
-    }
-    print('response:${response.name.toString()}');
-  }
+    socket.add(message);
 
-  static Future<ClientChannel> createCbjHubClient(String deviceIp) async {
-    await channel?.shutdown();
+    // listen for responses from the server
+    socket.listen(
+      // handle data from the server
+      (Uint8List data) {
+        final serverResponse = String.fromCharCodes(data);
+        print('Server: $serverResponse');
+      },
 
-    final String myPath =
-        '/home/guy/Documents/programming/git/CBJ_Smart-Home/CBJ_Hub/cbj_hub/lib/infrastructure/aioesphomeapi/cacert.pem';
-    final File f = File(myPath);
-    final Uint8List trustedRoot = f.readAsBytesSync();
+      // handle errors
+      onError: (error) {
+        print(error);
+        socket.destroy();
+      },
 
-    final channelCredentials =
-        ChannelCredentials.secure(certificates: trustedRoot);
-    final channelOptions = ChannelOptions(credentials: channelCredentials);
-
-    return channel = ClientChannel(
-      deviceIp,
-      port: 6053,
-      options: channelOptions,
+      // handle server ending connection
+      onDone: () {
+        print('Server left.');
+        socket.destroy();
+      },
     );
-
-    // final channel2 = ClientChannel(deviceIp,
-    //     port: 6053,
-    //     options: ChannelOptions(
-    //       credentials: ClientCertificateChannelCredentials(cert, key, pass),
-    //     ));
   }
-}
 
-class ClientCertificateChannelCredentials extends ChannelCredentials {
-  final List<int> _cert;
-  final List<int> _key;
-  final String _pass;
+  static Future<void> sendMessage(Socket socket, String message) async {
+    print('Client: $message');
+    socket.write(message);
 
-  const ClientCertificateChannelCredentials(
-    this._cert,
-    this._key,
-    this._pass,
-  ) : super.secure();
-
-  @override
-  SecurityContext get securityContext {
-    return SecurityContext(withTrustedRoots: true)
-      ..useCertificateChainBytes(_cert)
-      ..usePrivateKeyBytes(_key, password: _pass)
-      ..setAlpnProtocols(supportedAlpnProtocols, false);
+    await Future.delayed(const Duration(seconds: 2));
   }
 }
