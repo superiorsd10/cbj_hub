7894a2cfad735b33b23f7e7e253e1fb9a8d397a7
Guy Luz <guyluz11@gmail.com>
2021-07-14 19:15:11 +0300

    Working hello request to esp

--- cbj_hub/lib/infrastructure/aioesphomeapi/aioesphomeapi.dart
+++ cbj_hub/lib/infrastructure/aioesphomeapi/aioesphomeapi.dart
@@ -1,25 +1,49 @@
+import 'dart:convert';
 import 'dart:io';
 import 'dart:typed_data';
 
+import 'package:cbj_hub/infrastructure/gen/aioesphomeapi/protoc_as_dart/aioesphomeapi/api.pbserver.dart';
+
 class AioEspHomeApi {
   ///  Turn smart device on
-  static Future<void> createStreamWithHub(String addressToServer) async {
+  static Future<void> helloRequestToEsp(String addressToServer) async {
     // connect to the socket server
     final socket = await Socket.connect(addressToServer, 6053);
     print('Connected to: ${socket.remoteAddress.address}:${socket.remotePort}');
 
-    final message = Uint8List(8);
-    final bytedata = ByteData.view(message.buffer);
+    final HelloRequest helloRequest = HelloRequest(clientInfo: 'aioesphomeapi');
 
-    bytedata.setUint8(0, 0x00);
-    bytedata.setUint8(1, 0x07);
-    bytedata.setUint8(2, 0xFF);
-    bytedata.setUint8(3, 0x88);
-    bytedata.setUint8(4, 0x88);
-    bytedata.setUint8(5, 0x88);
-    bytedata.setUint8(6, 0x88);
-    bytedata.setUint8(7, 0x88);
+    const int numOfByteBeforeData = 3;
 
+    final List<int> list = utf8.encode(helloRequest.clientInfo);
+
+    final int totalSizeToTransfer = numOfByteBeforeData + list.length;
+
+    final Uint8List message = Uint8List(totalSizeToTransfer);
+
+    final ByteData byteData = ByteData.view(message.buffer);
+
+    /// First, a message in this protocol has a specific format:
+
+    /// A zero byte.
+    byteData.setUint8(0, 0x00);
+
+    /// VarInt denoting the size of the message object.
+    /// (type is not part of this)
+    byteData.setUint8(1, list.length);
+
+    ///  * VarInt denoting the type of message.
+    byteData.setUint8(2, 1);
+
+//  * The message object encoded as a ProtoBuf message
+
+    for (int i = numOfByteBeforeData;
+        i < list.length + numOfByteBeforeData;
+        i++) {
+      byteData.setUint8(i, list[i - numOfByteBeforeData]);
+    }
+
+    print(message);
     socket.add(message);
 
     // listen for responses from the server
