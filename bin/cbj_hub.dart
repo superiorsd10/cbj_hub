dc48c701e60cb903a548b71ad0eda7210518168d
Guy Luz <guyluz11@gmail.com>
2021-09-14 01:31:11 +0300

    Back to prod

--- bin/cbj_hub.dart
+++ bin/cbj_hub.dart
@@ -4,7 +4,7 @@ import 'package:cbj_hub/infrastructure/system_commands/device_pin_manager.dart';
 import 'package:cbj_hub/injection.dart';
 
 void main(List<String> arguments) async {
-  await configureInjection(Env.devPc);
+  await configureInjection(Env.prod);
 
   try {
     SharedVariables(arguments[0]);
