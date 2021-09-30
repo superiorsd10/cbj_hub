74cd1eafdf80fc51942646c3394e0c972c3532db
guy <guyluz11@gmail.com>
2021-09-30 11:30:40 +0300

    Small change in test file to check git is working

--- test/generic_test.dart
+++ test/generic_test.dart
@@ -1,7 +1,7 @@
 import 'package:test/test.dart';
 
 void main() {
-  test('DST example Test', () {
+  test('DST example Test 2', () {
     final DateTime today = DateTime(2020, 3, 8);
     final DateTime tomorrow = DateTime(2020, 3, 9);
     final diff = tomorrow.difference(today);
