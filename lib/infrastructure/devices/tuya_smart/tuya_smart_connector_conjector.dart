8a04f12a5ff766d16dd5ef847412ba074dbed74f
guy <guyluz11@gmail.com>
2021-09-30 12:18:56 +0300

    Add tuya code without implementation.

---
+++ lib/infrastructure/devices/tuya_smart/tuya_smart_connector_conjector.dart
@@ -0,0 +1,92 @@
+import 'dart:async';
+
+import 'package:cbj_hub/domain/generic_devices/abstract_device/core_failures.dart';
+import 'package:cbj_hub/domain/generic_devices/abstract_device/device_entity_abstract.dart';
+import 'package:cbj_hub/infrastructure/devices/tuya_smart/tuya_smart_white/tuya_smart_jbt_a70_rgbcw_wf_entity.dart';
+import 'package:cbj_hub/infrastructure/generic_devices/abstract_device/abstract_company_connector_conjector.dart';
+import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
+import 'package:multicast_dns/multicast_dns.dart';
+
+@singleton
+class TuyaSmartConnectorConjector implements AbstractCompanyConnectorConjector {
+  TuyaSmartConnectorConjector() {
+    _discoverNewDevices();
+  }
+
+  @override
+  static Map<String, DeviceEntityAbstract> companyDevices = {};
+
+  Future<void> _discoverNewDevices() async {}
+
+  @override
+  Future<Either<CoreFailure, Unit>> create(DeviceEntityAbstract tuya_smart) {
+    // TODO: implement create
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<CoreFailure, Unit>> delete(DeviceEntityAbstract tuya_smart) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> initiateHubConnection() {
+    // TODO: implement initiateHubConnection
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> manageHubRequestsForDevice(
+    DeviceEntityAbstract tuya_smartDE,
+  ) async {
+    final DeviceEntityAbstract? device =
+        companyDevices[tuya_smartDE.getDeviceId()];
+
+    if (device is TuyaSmartJbtA70RgbcwWfEntityEntity) {
+      device.executeDeviceAction(tuya_smartDE);
+    } else {
+      print('TuyaSmart device type does not exist');
+    }
+  }
+
+  @override
+  Future<Either<CoreFailure, Unit>> updateDatabase({
+    required String pathOfField,
+    required Map<String, dynamic> fieldsToUpdate,
+    String? forceUpdateLocation,
+  }) async {
+    // TODO: implement updateDatabase
+    throw UnimplementedError();
+  }
+
+  Future<String?> getIpFromMDNS(String deviceMdnsName) async {
+    final String name = '$deviceMdnsName.local';
+    final MDnsClient client = MDnsClient();
+    // Start the client with default options.
+    await client.start();
+
+    // Get the PTR record for the service.
+    await for (final PtrResourceRecord ptr in client
+        .lookup<PtrResourceRecord>(ResourceRecordQuery.serverPointer(name))) {
+      // Use the domainName from the PTR record to get the SRV record,
+      // which will have the port and local hostname.
+      // Note that duplicate messages may come through, especially if any
+      // other mDNS queries are running elsewhere on the machine.
+      await for (final SrvResourceRecord srv
+          in client.lookup<SrvResourceRecord>(
+        ResourceRecordQuery.service(ptr.domainName),
+      )) {
+        // Domain name will be something like "io.flutter.example@some-iphone.local._dartobservatory._tcp.local"
+        final String bundleId =
+            ptr.domainName; //.substring(0, ptr.domainName.indexOf('@'));
+        print(
+          'Dart observatory instance found at '
+          '${srv.target}:${srv.port} for "$bundleId".',
+        );
+      }
+    }
+    return null;
+  }
+}
---
+++ lib/infrastructure/devices/tuya_smart/tuya_smart_helpers.dart
@@ -0,0 +1,49 @@
+import 'package:cbj_hub/domain/generic_devices/abstract_device/device_entity_abstract.dart';
+import 'package:cbj_hub/domain/generic_devices/abstract_device/value_objects_core.dart';
+import 'package:cbj_hub/domain/generic_devices/generic_rgbw_light_device/generic_rgbw_light_value_objects.dart';
+import 'package:cbj_hub/infrastructure/devices/tuya_smart/tuya_smart_device_value_objects.dart';
+import 'package:cbj_hub/infrastructure/devices/tuya_smart/tuya_smart_white/tuya_smart_jbt_a70_rgbcw_wf_entity.dart';
+import 'package:cbj_hub/infrastructure/gen/cbj_hub_server/protoc_as_dart/cbj_hub_server.pbgrpc.dart';
+import 'package:yeedart/yeedart.dart';
+
+class TuyaSmartHelpers {
+  static DeviceEntityAbstract addDiscoverdDevice(
+    DiscoveryResponse tuya_smartDevice,
+  ) {
+    final TuyaSmartJbtA70RgbcwWfEntityEntity tuyaSmartDE =
+        TuyaSmartJbtA70RgbcwWfEntityEntity(
+      uniqueId: CoreUniqueId(),
+      defaultName: DeviceDefaultName(
+        tuya_smartDevice.name != ''
+            ? tuya_smartDevice.name
+            : 'TuyaSmart test 2',
+      ),
+      roomId: CoreUniqueId.newDevicesRoom(),
+      roomName: DeviceRoomName('Discovered'),
+      deviceStateGRPC: DeviceState(DeviceStateGRPC.ack.toString()),
+      senderDeviceOs: DeviceSenderDeviceOs('tuya_smart'),
+      senderDeviceModel: DeviceSenderDeviceModel('1SE'),
+      senderId: DeviceSenderId(),
+      compUuid: DeviceCompUuid('34asdfrsd23gggg'),
+      deviceMdnsName: DeviceMdnsName('yeelink-light-colora_miap9C52'),
+      lastKnownIp: DeviceLastKnownIp(tuya_smartDevice.address.address),
+      stateMassage: DeviceStateMassage('Hello World'),
+      powerConsumption: DevicePowerConsumption('0'),
+      tuya_smartDeviceId: TuyaSmartDeviceId(tuya_smartDevice.id.toString()),
+      tuya_smartPort: TuyaSmartPort(tuya_smartDevice.port.toString()),
+      lightSwitchState:
+          GenericRgbwLightSwitchState(tuya_smartDevice.powered.toString()),
+      lightColorTemperature: GenericRgbwLightColorTemperature(
+        tuya_smartDevice.colorTemperature.toString(),
+      ),
+      lightBrightness:
+          GenericRgbwLightBrightness(tuya_smartDevice.brightness.toString()),
+      lightColorAlpha: GenericRgbwLightColorAlpha('1.0'),
+      lightColorHue: GenericRgbwLightColorHue('0.0'),
+      lightColorSaturation: GenericRgbwLightColorSaturation('1.0'),
+      lightColorValue: GenericRgbwLightColorValue('1.0'),
+    );
+
+    return tuyaSmartDE;
+  }
+}
