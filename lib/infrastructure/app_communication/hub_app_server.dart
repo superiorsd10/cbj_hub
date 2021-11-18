3e39378acb3d032355d1dc29bae02c022815d7bf
guyluz11 <guyluz11@gmail.com>
2021-11-19 00:35:44 +0200

    Hub can now respond to get comp info request

--- lib/infrastructure/app_communication/hub_app_server.dart
+++ lib/infrastructure/app_communication/hub_app_server.dart
@@ -1,6 +1,9 @@
+import 'dart:io';
+
 import 'package:cbj_hub/domain/app_communication/i_app_communication_repository.dart';
 import 'package:cbj_hub/infrastructure/app_communication/app_communication_repository.dart';
 import 'package:cbj_hub/infrastructure/devices/device_helper/device_helper.dart';
+import 'package:cbj_hub/infrastructure/gen/cbj_hub_server/proto_gen_date.dart';
 import 'package:cbj_hub/infrastructure/gen/cbj_hub_server/protoc_as_dart/cbj_hub_server.pbgrpc.dart';
 import 'package:cbj_hub/infrastructure/generic_devices/abstract_device/device_entity_dto_abstract.dart';
 import 'package:cbj_hub/injection.dart';
@@ -39,17 +42,35 @@ class HubAppServer extends CbjHubServiceBase {
   }
 
   @override
+  Future<CompHubInfo> getCompHubInfo(
+    ServiceCall call,
+    CompHubInfo request,
+  ) async {
+    logger.i('Hub info got requested');
+
+    final CbjHubIno cbjHubIno = CbjHubIno(
+      deviceName: 'cbj Hub',
+      // pubspecYamlVersion: packageInfo.version,
+      protoLastGenDate: hubServerProtocGenDate,
+    );
+
+    final CompHubSpecs compHubSpecs = CompHubSpecs(
+      compOs: Platform.operatingSystem,
+    );
+
+    final CompHubInfo compHubInfo = CompHubInfo(
+      cbjInfo: cbjHubIno,
+      compSpecs: compHubSpecs,
+    );
+    return compHubInfo;
+  }
+
+  @override
   Stream<ClientStatusRequests> hubTransferDevices(
     ServiceCall call,
     Stream<RequestsAndStatusFromHub> request,
   ) async* {
     // TODO: implement registerHub
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<CompHubInfo> getCompHubInfo(ServiceCall call, CompHubInfo request) {
-    // TODO: implement getCompHubInfo
     throw UnimplementedError();
   }
 }
